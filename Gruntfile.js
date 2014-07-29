acdaf034f1e5a606d72140d92fe8aa75944be0c8
Alfiana Sibuea <alfian.sibuea@gmail.com>
2014-07-29 22:11:18 +0700

    Separate css from js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,19 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+
+        cssmin: {
+            minfy: {
+                files: {
+                    'build/marka.min.css': ['src/css/marka.css']
+                }
+            }
+        },
         
         jshint: {
             
             files: [
                 'gruntfile.js', 
-                'marka.js',
+                'src/**/*.js',
             ],
             
             options: {
@@ -19,21 +27,41 @@ module.exports = function(grunt) {
             options: {
                 mangle: true,
                 sourceMap: true,
-                sourceMapName: 'marka.min.js.map',
+                sourceMapName: 'build/marka.min.js.map',
                 preserveComments: 'some'
             },
             my_target: {
                 files: {
-                    'marka.min.js': ['marka.js']
+                    'build/marka.min.js': ['src/js/marka.js']
                 }
             }
         },
 
         copy: {
-			main: {
+            buildcss: {
+                expand : true,
+                cwd: 'src/css/',
+                src: '**',
+                dest: 'build/',
+            },
+            buildjs: {
+                expand : true,
+                cwd: 'src/js/',
+                src: 'marka.js',
+                dest: 'build/',
+            },
+			docjs: {
+                expand : true,
+                cwd: 'build/',
 				src: 'marka.js',
-				dest: 'docs/',
+				dest: 'docs/static/js',
 			},
+            doccss: {
+                expand : true,
+                cwd: 'build/',
+                src: 'marka.css',
+                dest: 'docs/static/css',
+            }
 		},
         
         watch: {
@@ -43,8 +71,8 @@ module.exports = function(grunt) {
                     spawn: false,
                     event: ['added', 'deleted', 'changed']
                 },
-                files: ['marka.js'],
-                tasks: ['jshint', 'uglify', 'copy']
+                files: ['src/**/*'],
+                tasks: ['cssmin', 'jshint', 'uglify', 'copy']
             },
             grunt: {
                 files: ['Gruntfile.js']
@@ -53,6 +81,7 @@ module.exports = function(grunt) {
     });
     
     // Load module
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
