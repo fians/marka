9e0f7f589d3aeb38874882b5bce7bfaa11659b15
Alfiana Sibuea <alfian.sibuea@gmail.com>
2014-08-30 16:26:32 +0700

    DOCS: Add popup to customize page

--- docs/static/js/customize.js
+++ docs/static/js/customize.js
@@ -120,6 +120,64 @@
 
 	}
 
+	/**
+	 * Generate customize download
+	 */
+	function download() {
+
+		var btn = $(this);
+		var loading = $('#custom #generate');
+
+		// Prevent action while generating file
+		if (!loading.hasClass('hide')) {
+			return false;
+		}
+
+		// Set header and core css
+		var core = $.ajax({
+			type: 'GET',
+			url: './static/marka/css/src/marka-core.css',
+			async: false
+		}).responseText + '\n';
+
+		var content = getHeader() + core;
+
+		// Get other css icon
+		for (var a = 0; a < choosenIcons.length; a++) {
+			content += $.ajax({
+				type: 'GET',
+				url: './static/marka/css/src/icons/icon-'+choosenIcons[a]+'.css',
+				async: false
+			}).responseText + '\n';
+		}
+
+		var minified = YAHOO.compressor.cssmin(content);
+
+		$('#compilePopup textarea.normal').text(content);
+		$('#compilePopup textarea.minified').text(minified);
+		$('#compilePopup').removeClass('hide');
+	}
+
+	/**
+	 * Choose custom compilation
+	 */
+	function chooseCustom() {
+		$('#compilePopup textarea').addClass('hide');
+		$('#compilePopup .choose').removeClass('selected');
+
+		$('#compilePopup textarea.'+$(this).data('type'))
+			.removeClass('hide');
+
+		$(this).addClass('selected');
+	}
+
+	/**
+	 * Close popup
+	 */
+	function closePopup() {
+		$('#compilePopup').addClass('hide');
+	}
+
 	$(document).on('ready', function() {
 
 		// Init icon
@@ -133,6 +191,11 @@
 
 		// Generate file
 		$('.generateCSS').on('click', generateCSS);
+
+		$('#download').on('click', download);
+		$('#compilePopup .choose').on('click', chooseCustom);
+		$('#compilePopup .wrapper').on('click', closePopup);
+		$('#compilePopup .close').on('click', closePopup);
 	});
 
 })(window, jQuery);
